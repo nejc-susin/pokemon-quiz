43a7f4bbf59bd1f25a74364107bc9ce29e1d1542
Nejc Susin <nejc.susin@gmail.com>
2022-02-18 13:10:42 +0100

    Project template

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  components: {HelloWorld}
+}
+</script>
+
+<template>
+  <HelloWorld msg="Hello world!"></HelloWorld>
+</template>
